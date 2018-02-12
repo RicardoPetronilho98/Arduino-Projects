4a4187cca93cb31953b54dee6914be0cc555677b
RicardoPetronilho98 <rpetronilho98@gmail.com>
2018-02-12 00:13:57 +0000

    adicionei novos conceitos

--- arduino_robot_car/arduino_robot_car.ino
+++ arduino_robot_car/arduino_robot_car.ino
@@ -9,12 +9,24 @@ int RECV_PIN = 2;
 IRrecv irrecv(RECV_PIN);
 decode_results results;
 
+// defines pins numbers
+const int trigPin = A3;
+const int echoPin = A2;
+long duration;
+int distance;
+
+// defines pins numbers
+const int trigPin2 = A5;
+const int echoPin2 = A4;
+long duration2;
+int distance2;
+
 int izqA = 5; 
 int izqB = 6; 
 int derA = 9; 
 int derB = 10; 
-int frontLed = 3;
-int vel = 175; // Velocidade dos motores (0-255)
+int frontLed = 4;
+int vel = 200; // Velocidade dos motores (0-255)
 char estado = 's'; // inicia detenido
 
 unsigned long currentMillis, previousMillis; 
@@ -33,11 +45,16 @@ void setup() {
   digitalWrite(frontLed, HIGH);
   
   irrecv.enableIRIn(); // start the receiver
+
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  pinMode(trigPin2, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin2, INPUT); // Sets the echoPin as an Input
 } 
 
 void loop() { 
 
-  if (irrecv.decode(&results)){
+  /*if (irrecv.decode(&results)){
 
     unsigned int value = results.value;
 
@@ -50,11 +67,11 @@ void loop() {
     else estado = 's';
 
     irrecv.resume(); //receive the next value
-  }
+  }*/
 
   //currentMillis = millis(); 
   
-  //if (Serial.available() > 0) estado = Serial.read();
+  if (Serial.available() > 0) estado = Serial.read();
 
   /* girar automaticamente:
   if (estado == 'r' && start == 1){
@@ -67,6 +84,58 @@ void loop() {
   //countMillis();
   
   atualizaEstado();  
+
+  atualizaDistancia();
+  atualizaDistancia2();
+}
+
+void atualizaDistancia(){
+
+  // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  
+  // Calculating the distance
+  distance = duration*0.034/2;
+  
+  // Prints the distance on the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.println(distance);  
+  
+  if (distance < 30 && estado == 'f') estado = 's';
+}
+
+
+void atualizaDistancia2(){
+
+  // Clears the trigPin
+  digitalWrite(trigPin2, LOW);
+  delayMicroseconds(2);
+  
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin2, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin2, LOW);
+  
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration2 = pulseIn(echoPin2, HIGH);
+  
+  // Calculating the distance
+  distance2 = duration2 * 0.034 / 2;
+  
+  // Prints the distance on the Serial Monitor
+  Serial.print("Distance2: ");
+  Serial.println(distance2);  
+  
+  if (distance2 < 30 && estado == 'f') estado = 's';
 }
 
 /*
